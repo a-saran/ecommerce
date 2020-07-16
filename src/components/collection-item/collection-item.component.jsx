83164b3e4a49fdb4a347e01c82bb3d2eae75a276
asaran97 <asaran234@gmail.com>
2020-07-16 22:28:10 +0530

    added collection-item component

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./collection-item.styles.scss";
+
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="collection-footer">
+        <div className="name">{name}</div>
+        <div className="price">{price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CollectionItem;
--- src/components/preview-collection/preview-collection.component.js
+++ src/components/preview-collection/preview-collection.component.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 import "./preview-collection.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items.slice(0, 4).map(({ name, id }) => (
-          <div key={id}>{name}</div>
+        {items.slice(0, 4).map(({ id, ...itemProps }) => (
+          <CollectionItem key={id} {...itemProps} />
         ))}
       </div>
     </div>
