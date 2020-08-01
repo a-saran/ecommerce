a93b5e75a0328ecd73e3d0e00818d93c0cd76f60
asaran97 <asaran234@gmail.com>
2020-08-01 19:55:20 +0530

    created cart item component

---
+++ src/components/cart-item/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.scss";
+
+const CartItem = ({ item: { name, imageUrl, price, quantity } }) => {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt={name} />
+      <div className="item-details">
+        <div className="name">{name}</div>
+        <div className="price">
+          {quantity} x {price}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/components/cart/cart-dropdown/index.js
+++ src/components/cart/cart-dropdown/index.js
@@ -1,12 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
+
 import CustomButton from "../../custom-button/custom-button.component";
+import CartItem from "../../cart-item/index";
 import "./styles.scss";
 
-const CartDropdown = ({ isOpen }) => (
+const CartDropdown = ({ isOpen, cartItems }) => (
   <div className={`cart-dropdown${isOpen ? " open" : ""}`}>
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map(item => (
+        <CartItem item={item} key={item.id} />
+      ))}
+    </div>
     <CustomButton>Go To Checkout</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
