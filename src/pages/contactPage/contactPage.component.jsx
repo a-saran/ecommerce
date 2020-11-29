1565a6b2ff82aad83f7c4fdaea66b1ef570f11d9
asaran97 <asaran234@gmail.com>
2020-11-29 23:09:05 +0530

    contact component

--- src/pages/contactPage/contactPage.component.jsx
+++ src/pages/contactPage/contactPage.component.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./styles.scss";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="contact-page">
+      <h3 className="heading">ContactPage</h3>
+      <h4>This is a Demo App.</h4>
+      <h4>
+        Developed by{" "}
+        <a href="http://saran-a.dev/" target="_blank">
+          Saran
+        </a>
+      </h4>
+    </div>
+  );
 };
 
 export default ContactPage;
