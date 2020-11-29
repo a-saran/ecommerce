ae65068dc5927928e787c7706fd32151d1281433
asaran97 <asaran234@gmail.com>
2020-11-29 12:40:13 +0530

    added link to topics

--- src/components/preview-collection/preview-collection.component.js
+++ src/components/preview-collection/preview-collection.component.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./preview-collection.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, routeName }) => {
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <h1 className="title">
+        <Link to={`/shop/${routeName.toLowerCase()}`}>
+          {title.toUpperCase()}
+        </Link>
+      </h1>
       <div className="preview">
         {items.slice(0, 4).map(item => (
           <CollectionItem key={item.id} item={item} />
