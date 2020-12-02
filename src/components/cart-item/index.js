1f49f2f52d15920269c0e3b39387f4c0a37a0142
asaran97 <asaran234@gmail.com>
2020-12-02 15:32:27 +0530

    optimaization

--- src/components/cart-item/index.js
+++ src/components/cart-item/index.js
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { memo } from "react";
 import "./styles.scss";
 
-const CartItem = ({ item: { name, imageUrl, price, quantity } }) => {
-  return (
-    <div className="cart-item">
-      <img src={imageUrl} alt={name} />
-      <div className="item-details">
-        <div className="name">{name}</div>
-        <div className="price">
-          {quantity} x {price}
-        </div>
+const CartItem = ({ item: { name, imageUrl, price, quantity } }) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt={name} />
+    <div className="item-details">
+      <div className="name">{name}</div>
+      <div className="price">
+        {quantity} x {price}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
-export default CartItem;
+export default memo(CartItem);
